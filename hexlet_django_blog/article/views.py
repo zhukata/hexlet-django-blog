393c807859dc95032341d4f98c1a2991ef973bf6
zhukata <zhukata@gmail.com>
2024-07-08 20:53:50 +0300

    reverse, redirect

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.urls import reverse
+from django.views import View
 
-def index(request):
-    return render(request, 'articles/index.html', context={
-        'name_app': 'articles',
-    })
+class IndexView(View):
+
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        return redirect(reverse('article', kwargs={'tags': 'python', 'article_id': 42}))
+
+def index(request, tags, article_id):
+    return HttpResponse(f"Cтрока номер {article_id}. Тег {tags}")
--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -20,8 +20,8 @@ from hexlet_django_blog import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.IndexView.as_view()),
     path('about/', views.about),
-    path('articles/', include('hexlet_django_blog.article.urls')),
+    path('article/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
 ]
